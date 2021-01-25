6666125e9a1a255d96543a0c0cc6540f2b58bf1f
CarolusAntonius <71439668+CarolusAntonius@users.noreply.github.com>
2021-01-25 13:59:49 +0100

    Placeholder website and Javascript test

---
+++ js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function funktion(a, b) {
+  return a + b;
+}
+
+//Antingen:
+
+//let vardeAttSkrivaUt = funktion(2, 3);
+//console.log(vardeAttSkrivaUt);
+
+//eller:
+
+console.log(funktion(2, 3));
