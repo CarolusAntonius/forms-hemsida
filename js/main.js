235e30e90dedc2af1e3601e7bf395d7c10f53de6
CarolusAntonius <71439668+CarolusAntonius@users.noreply.github.com>
2021-02-01 13:38:27 +0100

    Experimenting with new JavaScript things that I have learned and printing them out.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 "use strict";
 
+const projectName = "Greatest Battles of Karl XII Game Quiz";
+const projectVersion = 1.0;
+console.log(projectName);
+console.log(projectName.split(" "));
+console.log(projectName.split(""));
+console.log(projectName.trim().Length); //Trim tar bort alla mellanslag
+console.log(projectName.toUpperCase);
+console.log(projectName.toLowerCase);
+
+let score = 0;
+
+//gör en trigger:
+
+score = score + 1;
+console.log("Increased score by 1 point");
+console.log("Your score is now " + score);
+console.log(typeof score);
+
 function funktion(a, b) {
   return a + b;
 }
@@ -12,3 +30,9 @@ function funktion(a, b) {
 //eller:
 
 console.log(funktion(2, 3));
+
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+greet();
