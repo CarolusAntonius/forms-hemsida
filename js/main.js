ec1ec7140d51867eb2abae8ddfa189c86cc6dc7e
CarolusAntonius <71439668+CarolusAntonius@users.noreply.github.com>
2021-02-22 15:32:33 +0100

    SPA implementation and quiz added

--- js/main.js
+++ js/main.js
@@ -1,298 +1,103 @@
 "use strict";
 
-let firstName = "Dylan";
-let lastName = "Israel";
+// 3. document.querySelector och document.querySelectorAll
+// 6. classList.toggle
 
-let fullName = firstName + " " + lastName;
-const fullNameTwo = `${firstName} ${lastName}`;
-
-let example = 7.77; //man kan även lägga det i en string och det fungerar ändå, let example = "7.77";
-
-console.log(example);
-console.log(parseFloat(example));
-console.log(parseInt(example));
-console.log(example.toFixed(5)); //ange antal decimaler
-
-console.log(fullName);
-
-const projectName = "Greatest Battles of Karl XII Game Quiz";
-const projectVersion = 1.0;
-console.log(projectName);
-console.log(projectName.split(" "));
-console.log(projectName.split(""));
-console.log(projectName.trim().Length); //Trim tar bort alla mellanslag
-console.log(projectName.toUpperCase);
-console.log(projectName.toLowerCase);
-
-let score = 0;
-
-//gör en trigger:
-
-score = score + 1;
-console.log("Increased score by 1 point");
-console.log("Your score is now " + score);
-console.log(typeof score);
-
-function funktion(a, b) {
-  return a + b;
-}
-
-//Antingen:
-
-//let vardeAttSkrivaUt = funktion(2, 3);
-//console.log(vardeAttSkrivaUt);
-
-//eller:
-
-console.log(funktion(2, 3));
-
-function greet(name) {
-  console.log("Hello " + name);
-}
-
-greet();
-
-let example1 = [5, 7, 6];
-
-example1.push(8, 9, 10);
-example1.pop();
-
-example1[0] = 1;
-
-example1.forEach((element) => {
-  //ARROW FUNCTIONS!!!
-  console.log(element);
-});
-
-console.log(example1);
-
-const products = ["toilet paper", "bottled water", "sanitizer"];
-products[0] = "paper towels";
-products.pop();
-products.push("bleach");
-
-console.log(products);
-
-//OBJEKT:
-
-let example2 = {
-  firstName: "Dylan", //Key: value
-  lastName: "Israel",
-  address: {
-    city: "Austin",
-    state: "Texas",
-  },
-  age: 30,
-  cats: ["Milo", "Tito", "Achieles"],
-};
-
-example2.age = 31;
-
-console.log(example1.age);
-console.log(Object.keys(example1));
-console.log(Object.values(example1));
-console.log(example2.address.city);
-console.log(example2.hasOwnProperty("firstName")); //kolla om example1 har en string med namnet firstname
-
-const user = {
-  firstName: "Carl-Anton",
-  lastName: "Grandelius",
-  address: {
-    city: "Stockholm",
-    county: "Stockholms laen",
-  },
-  hobbies: ["running", "swimming"],
-  isGoldMember: "true",
-};
-
-console.log(user);
-
-//ELLER:
-
-/*
-  const user = {
-    firstName: 'Dylan',
-    lastName: 'Israel'
-};
-
-user['address'] = {
-    city: 'Tampa',
-    state: 'Florida'
-};
-
-user.hobbies = ['Anime', 'Coding', 'Dating', 'Gaming'];
-user.isGoldMember = true;
-
-console.log(user);
-
-*/
+//document.querySelectorAll(".fraga-container").forEach((a) => (a.style.display = "none"));
 
 /*
 
-console.log(example1 == example2); //kolla att de är lika
-console.log(example1 === example2); //kolla att de är lika och att typen är lika
+  let activeId = 0;
 
-console.log(example1 != example2); //kolla att de är olika
-console.log(example1 !== example2); //kolla att de är olika och att typen är lika
+
+function nextQuestion(){
+    activeId += 1;
+    console.log(activeId);
+    displayQuestion(activeId);
+}
+
+function displayQuestion(activeId){
+  console.log(activeId);
+  //document.querySelector("#hej" + activeId).classList.toggle("invisible"); //man kan inte börja id med en siffra
+}
+
+//document.createElement("div");
+
+function skapaKnapp() {
+  let btn = document.createElement("BUTTON");
+  btn.innerHTML = "CLICK ME";
+  document.body.appendChild(btn);
+}
+
+
+}
+
+//ÖVRIGT:::
 
 */
 
-let example3 = 1;
-example3 %= 5; //*=, +=, -= osv.
+update();
 
-console.log(example1);
+function update() {
+  let articles = document.querySelectorAll("article"); //skapa array och fyll den med alla elemen med namnet "article"
 
-const customerAnswer = Math.random() >= 0.5;
-let quantity = 2;
-
-if (customerAnswer) {
-  quantity++;
-  // quantity = quantity + 1;
-}
-
-console.log(quantity);
-
-//eller:
-
-let slumpvarde = Math.random(); //math.random returnerar alltid ett värde från 0 till 1
-
-let varde = 2;
-
-if (slumpvarde >= 0.5) {
-  varde++;
-}
-console.log(varde);
-
-let example4 = 5;
-
-if (example4 === 6 || false === true) {
-  console.log("Runs");
-} else if (true) {
-  console.log("else if");
-} else {
-  console.log("else");
-}
-
-const age = 18;
-
-if (age <= 12) {
-  console.log("child");
-} else if (age <= 20) {
-  console.log("teenager");
-} else {
-  console.log("adult");
-}
-
-let studentAnswer = "D";
-
-switch (studentAnswer) {
-  case "A":
-    console.log("A is wrong.");
-    break;
-  case "B":
-    console.log("B is wrong.");
-    break;
-  case "C":
-    console.log("C is correct.");
-    break;
-  default:
-    console.log("Not a real answer.");
-}
-
-const userType = null;
-
-switch (userType) {
-  case "guest":
-    console.log("You are a guest");
-    break;
-  case "shopper":
-    console.log("You are a shopper");
-    break;
-  case "super shopper":
-    console.log("You are a super shopper");
-    break;
-  default:
-    console.log("We asume you are a guest");
-    break;
-}
-
-let kvantitet = 5;
-let pris = 3;
-let total = 0;
-
-for (let i = 1; i < kvantitet + 1; i++) {
-  total += pris;
-}
-console.log(total);
-
-//eller
-/*
-let price = [3, 3, 3, 3, 3];
-let total = 0;
-
-for (let i = 1; i < price.length; i++) {
-  total += price;
-}
-console.log(total);
-*/
-
-// Challenge: Total Price
-
-/*
- 1. Give our customer their total price. 
- 2. Add the price of the items together with a sales tax of 8%.
- 3. Log the price. 
-*/
-
-const cartItems = [
-  { quantity: 1, price: 5 },
-  { quantity: 3, price: 4 },
-  { quantity: 10, price: 1 },
-];
-let totalsum = 0;
-
-// for(let i = 0; i < cartItems.length; i++) {
-//     total += cartItems[i].price * cartItems[i].quantity;
-// }
-
-for (const cartItem of cartItems) {
-  totalsum += cartItem.price * cartItem.quantity;
-}
-
-// total = total * 1.08;
-totalsum *= 1.08;
-
-console.log(total);
-
-let count = 0;
-
-do {
-  count++;
-
-  if (count < 20) {
-    break;
+  for (let i = 0; i < articles.length; i++) {
+    articles[i].classList.add("invisible"); //lägg till klassen invisible till alla element ked namnet "articles"
   }
-} while (false);
 
-let update = 0;
+  let nuvarande = document.querySelector("input:checked").value; //fyll nuvarande med värdet på den första ikryssade inputen.value value heter samma som id:t längre ner...
 
-do {
-  update++;
-  console.log(update);
-} while (update > 20);
-
-console.log(update);
-
-function lax() {
-  return 10;
+  document.querySelector("#" + nuvarande).classList.toggle("invisible"); //hitta artikeln med id:t #nuvarande och stäng av invisible
 }
 
-console.log(lax());
+let nuvarandefraga = 1;
 
-//eller
-
-function lax(meningslostnummer) {
-  return 10;
+function changeQuestion() {
+  if (nuvarandefraga < 3) {
+    document
+      .querySelector("#fraga" + nuvarandefraga)
+      .classList.toggle("invisible");
+    nuvarandefraga += 1;
+    document
+      .querySelector("#fraga" + nuvarandefraga)
+      .classList.toggle("invisible");
+  } else {
+    document
+      .getElementById("changeQuestion-button")
+      .classList.toggle("invisible");
+    document.getElementById("submitQuiz-button").classList.toggle("invisible");
+    document
+      .querySelector("#fraga" + nuvarandefraga)
+      .classList.toggle("invisible");
+    console.log("inaktiverat");
+  }
 }
 
-console.log(lax(2));
+function submitQuiz() {
+  let points = 0;
+
+  let answers = document.querySelectorAll("[data-question]:checked");
+  console.log(answers);
+
+  // Loopa igenom svaren och lägg till deras "value" till points:
+  for (let index = 0; index < answers.length; index++) {
+    points += parseInt(answers[index].value);
+  }
+
+  // Lägg in poängen i vår "correct-answers" span:
+  document.querySelector(".correct-answers").innerHTML = points;
+
+  //Göm frågorna och visa resultatet: !!!!!!!!!!!!!!!!
+  document.querySelector(".questions").classList.toggle("invisible");
+  document.querySelector(".result").classList.toggle("invisible");
+}
+
+function resetQuiz() {
+  //Göm resultatet och visa frågorna:
+  document.querySelector(".result").classList.toggle("invisible");
+  document.querySelector(".questions").classList.toggle("invisible");
+
+  //Nollställ formuläret:
+  document.querySelector(".questions").reset();
+  document.querySelector("#fraga1").classList.toggle("invisible");
+}
