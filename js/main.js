99dc646dc33be43ab2d135280ae759b6e9d96287
CarolusAntonius <71439668+CarolusAntonius@users.noreply.github.com>
2021-02-25 23:23:48 +0100

    Optimization - removal of unused code and pictures

--- js/main.js
+++ js/main.js
@@ -1,53 +1,17 @@
 "use strict";
 
-// 3. document.querySelector och document.querySelectorAll
-// 6. classList.toggle
-
-//document.querySelectorAll(".fraga-container").forEach((a) => (a.style.display = "none"));
-
-/*
-
-  let activeId = 0;
-
-
-function nextQuestion(){
-    activeId += 1;
-    console.log(activeId);
-    displayQuestion(activeId);
-}
-
-function displayQuestion(activeId){
-  console.log(activeId);
-  //document.querySelector("#hej" + activeId).classList.toggle("invisible"); //man kan inte börja id med en siffra
-}
-
-//document.createElement("div");
-
-function skapaKnapp() {
-  let btn = document.createElement("BUTTON");
-  btn.innerHTML = "CLICK ME";
-  document.body.appendChild(btn);
-}
-
-
-}
-
-//ÖVRIGT:::
-
-*/
-
 update();
 
 function update() {
-  let articles = document.querySelectorAll("article"); //skapa array och fyll den med alla elemen med namnet "article"
+  let articles = document.querySelectorAll("article");
 
   for (let i = 0; i < articles.length; i++) {
-    articles[i].classList.add("invisible"); //lägg till klassen invisible till alla element ked namnet "articles"
+    articles[i].classList.add("invisible");
   }
 
-  let nuvarande = document.querySelector("input:checked").value; //fyll nuvarande med värdet på den första ikryssade inputen.value value heter samma som id:t längre ner...
+  let nuvarande = document.querySelector("input:checked").value;
 
-  document.querySelector("#" + nuvarande).classList.toggle("invisible"); //hitta artikeln med id:t #nuvarande och stäng av invisible
+  document.querySelector("#" + nuvarande).classList.toggle("invisible");
 }
 
 let nuvarandefraga = 1;
@@ -76,8 +40,6 @@ function changeQuestion() {
   }
 }
 
-//GÅR INTE ATT GÖRA SEPARAT FUNKTION MED nextQuestion() eftersom nuvarande fråga i så fall måste returneras till changeQuestion
-
 function submitQuiz() {
   document
     .querySelector("#fraga" + nuvarandefraga)
@@ -87,7 +49,6 @@ function submitQuiz() {
   let answers = document.querySelectorAll("[data-question]:checked");
   console.log(answers);
 
-  // Loopa igenom svaren och lägg till deras "value" till points:
   for (let index = 0; index < answers.length; index++) {
     points += parseInt(answers[index].value);
   }
@@ -104,38 +65,29 @@ function submitQuiz() {
     niva = "Menig soldat";
   }
 
-  // Lägg in poängen i vår "correct-answers" span:
   document.querySelector(".correct-answers").innerHTML = points;
   document.querySelector(".niva").innerHTML = niva;
 
-  //Göm frågorna och visa resultatet: !!!!!!!!!!!!!!!!
   document.querySelector(".questions").classList.toggle("invisible");
   document.querySelector(".result").classList.toggle("invisible");
 
-  //göm headern
   document.querySelector("header").classList.toggle("invisible");
-  //göm footern
   document.querySelector("footer").classList.toggle("invisible");
 }
 
 function resetQuiz() {
-  //Göm resultatet och visa frågorna:
   document.querySelector(".result").classList.toggle("invisible");
   document.querySelector(".questions").classList.toggle("invisible");
 
-  //Nollställ formuläret:
   document.querySelector(".questions").reset();
   document.querySelector("#fraga1").classList.toggle("invisible");
 
-  //Byt fråga
   document
     .getElementById("changeQuestion-button")
     .classList.toggle("invisible");
   document.getElementById("submitQuiz-button").classList.toggle("invisible");
   nuvarandefraga = 1;
 
-  //visa headern
   document.querySelector("header").classList.toggle("invisible");
-  //visa footern
   document.querySelector("footer").classList.toggle("invisible");
 }
